d45f9711d12e24f3c85e30851655df26cfba60d8
Jaycee Baste <60146301+jacobcharlesbaste@users.noreply.github.com>
2023-04-15 13:07:24 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../demo-styling.css']
 })
-export class AppComponent {
-  title = 'angular-quickstart';
+
+export class AppComponent implements OnInit {  
+  constructor(  
+    private metaTagService: Meta  
+  ) { }  
+    
+  ngOnInit() {  
+    this.metaTagService.addTags([  
+      { name: 'verify-admitad', content: '5e9108d548' },  
+    ]);  
+        
+  }  
 }
